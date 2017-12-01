8c5f978a5dc7c02f0011b44d6532befb022b16d4
朱瑞恒 <18749830459@163.com>
2017-12-01 14:30:08 +0800

    文字轮播

--- pages/index/index.js
+++ pages/index/index.js
@@ -38,14 +38,7 @@ Page({
         currentPage:0,
         isLoading:false,
         navto: 1,  //是否跳转启动页 1跳转 0 不跳转
-        marqueePace: 1,//滚动速度
-        marqueeDistance: 0,//初始滚动距离
-        marqueeDistance2: 0,
-        marquee2copy_status: false,
-        marquee2_margin: 60,
-        size: 14,
-        orientation: 'left',//滚动方向
-        interval: 40 // 时间间隔
+        broadcasting:'肌肤有困难？就找禾葡兰！千名专业美肤导师为你提供一对一服务，随时免费咨询，微信搜索小程序：禾葡兰护肤中心'
     },
     onShow:function(){
       let that = this;
@@ -85,68 +78,8 @@ Page({
             }
           }
         })
-        var vm = this;
-        console.log("length1:", vm.data.broadcasting.length);
-        console.log("length2:", vm.data.size);
-        var length = vm.data.broadcasting.length * vm.data.size;//文字长度
-        console.log("length:", length);
-        var windowWidth = wx.getSystemInfoSync().windowWidth;// 屏幕宽度
-        console.log("windowWidth:", windowWidth);
-        console.log("res:", vm.data.marquee2_margin)
-        vm.setData({
-          length: length,
-          windowWidth: windowWidth,
-          marquee2_margin: length < windowWidth ? windowWidth - length : vm.data.marquee2_margin//当文字长度小于屏幕长度时，需要增加补白
-        });
-        vm.run1();// 水平一行字滚动完了再按照原来的方向滚动
-        vm.run2();// 第一个字消失后立即从右边出现
         wx.hideLoading()
       
-    },
-    // 方法1
-    run1: function () {
-      var vm = this;
-      var interval = setInterval(function () {
-        if (-vm.data.marqueeDistance < vm.data.length) {
-          vm.setData({
-            marqueeDistance: vm.data.marqueeDistance - vm.data.marqueePace,
-          });
-        } else {
-          clearInterval(interval);
-          vm.setData({
-            marqueeDistance: vm.data.windowWidth
-          });
-          vm.run1();
-        }
-      }, vm.data.interval);
-    },
-    // 方法1
-    run2: function () {
-      var vm = this;
-      var interval = setInterval(function () {
-        //console.log("marquee2_margin",vm.data.marquee2_margin);
-        if (-vm.data.marqueeDistance2 < vm.data.length) {
-          // 如果文字滚动到出现marquee2_margin=30px的白边，就接着显示
-          vm.setData({
-            marqueeDistance2: vm.data.marqueeDistance2 - vm.data.marqueePace,
-            marquee2copy_status: vm.data.length + vm.data.marqueeDistance2 <= vm.data.windowWidth + vm.data.marquee2_margin,
-          });
-        } else {
-          if (-vm.data.marqueeDistance2 >= vm.data.marquee2_margin) { // 当第二条文字滚动到最左边时
-            vm.setData({
-              marqueeDistance2: vm.data.marquee2_margin // 直接重新滚动
-            });
-            clearInterval(interval);
-            vm.run2();
-          } else {
-            clearInterval(interval);
-            vm.setData({
-              marqueeDistance2: -vm.data.windowWidth
-            });
-            vm.run2();
-          }
-        }
-      }, vm.data.interval);
     },
     loadRecommend:function () {
         let that =this;
